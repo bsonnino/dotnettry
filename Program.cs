a76fae17003e968a25ce40e726a8615d7c24fe0f
Bruno Sonnino <sonnino@revolution.com.br>
2020-03-22 16:48:34 -0400

    Add files

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace dotnettrypost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < args.Length; i++)
+                if (args[i] == "--region")
+                {
+                    if (args[i + 1] == "HelloWorld")
+                        HelloWorld();
+                    else if (args[i + 1] == "ByeBye")
+                        ByeBye();
+                }
+        }
+
+        private static void HelloWorld()
+        {
+            #region HelloWorld
+            Console.WriteLine("Hello World!");
+            #endregion
+        }
+
+        private static void ByeBye()
+        {
+            #region ByeBye
+            Console.WriteLine("ByeBye!");
+            #endregion
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Documenting your code with **dotnet try**
+## Introduction
+To document your code with dotnet try, you must mix your markdown text with some fences
+
+Below, you should see the code in the program.cs file:
+```cs --source-file ./Program.cs --project ./dotnettrypost.csproj --region HelloWorld
+```
+
+Below you should see the same code snippet, but not editable:
+```cs --source-file ./Program.cs --project ./dotnettrypost.csproj --region HelloWorld --editable false
+```
